f17ff1047e19b28802d75e5a3a07fc2996cfb6f0
Ezra Asquith <Ezra.asquith@gmail.com>
2024-08-13 19:22:46 -0700

    added a ui

--- script.js
+++ script.js
@@ -21,39 +21,37 @@ function computerPlay() {
     }
 }
 
-function humanPlay() {
-    let play = prompt("Enter (r)ock (p)apper (s)cissors")
-
-    if (play === "r") {
-        return "rock"
-    } else if(play === "p") {
-        return "paper"
-    } else {
-        return "scissors"
-    }
-
+function humanPlay(play) {
+    return play
 }
 
 function determineWinner(player1, player2) {
     if(player1 === player2) {
+        resultsText.textContent = "You tied"
         console.log("You tied")
         return 0
     } else if(player1 === "rock" && player2 === "paper") {
+        resultsText.textContent = "You played rock, the computer played paper"
         console.log("You played rock, the computer played paper")
         return 2
     } else if(player1 === "rock" && player2 === "scissors") {
+        resultsText.textContent = "You played rock, the computer played scissors"
         console.log("You played rock, the computer played scissors")
         return 1
     } else if(player1 === "paper" && player2 === "rock") {
+        resultsText.textContent = "You played paper, the computer played rock"
         console.log("You played paper, the computer played rock")
         return 1
     } else if(player1 === "paper" && player2 === "scissors") {
+        resultsText.textContent = "You played paper, the computer played scissors"
         console.log("You played paper, the computer played scissors")
         return 2
     } else if(player1 === "scissors" && player2 === "rock") {
+        resultsText.textContent = "You played scissors, the computer played rock"
         console.log("You played scissors, the computer played rock")
         return 2
     } else if(player1 === "scissors" && player2 === "paper") {
+        resultsText.textContent = "You played scissors, the computer played paper"
         console.log("You played scissors, the computer played paper")
         return 1
     } else {
@@ -61,26 +59,54 @@ function determineWinner(player1, player2) {
     }
 }
 
-function playRound() {
-    let human = humanPlay()
+function playRound(play) {
+    let human = humanPlay(play)
     let computer = computerPlay()
-    return determineWinner(human, computer)
-}
-
-function playGame(rounds) {
-    let humanScore = 0;
-    let computerScore = 0;
-    for(let round = 0; round <= rounds; round++) {
-        let results = playRound()   
-        if(results === 1) {
-            humanScore++ }
-        if(results === 2) {
-            computerScore++ }
+    let winner = determineWinner(human, computer);
+    if(winner === 1) {
+        humanScore++ }
+    if(winner === 2) {
+        computerScore++ }
+    if(humanScore === 5) {
+        resultsText.textContent = "CONGRATS, YOU WON!!! :)"
+        humanScore = 0;
+        computerScore = 0;
     }
-    console.log("Your Score: " + humanScore)
-    console.log("Computer Score: " + computerScore)
+    if(computerScore === 5){
+        resultsText.textContent = "CONGRATS, YOU LOST, KILL YOURSELF!!! :("
+        humanScore = 0;
+        computerScore = 0;
+    }
+    scoreText.textContent = "Your score:" + humanScore + " - Computer Score: " + computerScore
 }
 
-playGame(5)
 
+
+//DOM Code
+let humanScore = 0;
+let computerScore = 0;
+const container = document.querySelector(".container");
+const scoreText = document.createElement("div");
+const resultsText = document.createElement("div");
+
+container.appendChild(scoreText)
+container.appendChild(resultsText)
+
+
+
+const rockButton = document.querySelector(".rockButton");
+const paperButton = document.querySelector(".paperButton");
+const scissorsButton = document.querySelector(".scissorsButton");
+
+rockButton.addEventListener("click", () => {
+    playRound("rock");
+});
+
+paperButton.addEventListener("click", ()=> {
+    playRound("paper")
+})
+
+scissorsButton.addEventListener("click", () => {
+    playRound("scissors")
+})
 
